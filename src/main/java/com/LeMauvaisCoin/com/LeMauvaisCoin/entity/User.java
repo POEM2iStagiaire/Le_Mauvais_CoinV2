ce9915cbeea53e64e2fe4ec4e8b51d06163c920d
Geoffrey CABASSON <geoffrey.cabasson@neuf.fr>
2023-02-20 11:02:31 +0100

    modification pom.xml

--- src/main/java/com/LeMauvaisCoin/com/LeMauvaisCoin/entity/User.java
+++ src/main/java/com/LeMauvaisCoin/com/LeMauvaisCoin/entity/User.java
@@ -1,5 +1,32 @@
 package com.LeMauvaisCoin.com.LeMauvaisCoin.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name="users")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
 public class User {
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String login;
+	private String password;
+	@Column(name="connection_number")
+	private int connectionNumber;
+	
+	
+	
+	
+	
 
 }
