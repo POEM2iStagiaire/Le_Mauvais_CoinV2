afffc66cd71fef3df44bc90fc758df26e7fdeec1
Geoffrey CABASSON <geoffrey.cabasson@neuf.fr>
2023-02-20 11:52:21 +0100

    insertion des classes

---
+++ src/main/java/com/LeMauvaisCoin/com/LeMauvaisCoin/entity/UserInformation.java
@@ -0,0 +1,36 @@
+package com.LeMauvaisCoin.com.LeMauvaisCoin.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Entity
+@Table//(name="user_informations")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class UserInformation {
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String addresse;
+	private String city;
+	private String email;
+	//@Column(name="phone_number")
+	private int phoneNumber;
+	
+	
+	@OneToOne @JoinColumn(name="idUser")
+	//@Column(name="id_user")
+	private User user;
+	
+}
+
+
+
+
